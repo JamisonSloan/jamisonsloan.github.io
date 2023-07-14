351f0d8c591b38565aef438a8c811ccf971868af
Haley <haleypark.design@gmail.com>
2023-07-14 10:52:50 -0400

    removed cv link for now, fixed the writing page that hasn't been updated, added misc navigation related scripts

--- __site/assets/scripts/script.js
+++ __site/assets/scripts/script.js
@@ -1,68 +1,62 @@
+// Takes sub-navigation elements and puts them under the right parent navigation element
+const subNavElems = document.querySelectorAll(".sub-navigation");
+const navDiv = document.querySelector(".navigation");
+
+for (const subNavElem of subNavElems) {
+  const parentPage = subNavElem.getAttribute("href").split("/")[1];
+  const parentPageNavElem = document.getElementById(parentPage);
+  navDiv.removeChild(subNavElem);
+  navDiv.insertBefore(subNavElem, findNextNavElem(parentPageNavElem));
+}
+
+// Finds the next navigation element after the parent page that's not a sub-navigation element
+function findNextNavElem(parentPageNavElem) {
+  const navItems = document.querySelectorAll(".navigation a");
+  const parentPageNavIndex = Array.from(navItems).indexOf(parentPageNavElem);
+
+  for (let i = parentPageNavIndex + 1; i < navItems.length; i++) {
+    if (!navItems[i].classList.contains("sub-navigation")) {
+      return navItems[i];
+    }
+  }
+}
+
 // Selects current page on the navigation bar
-
 const pageTitle = document.title.toLowerCase().replace(/\s/g, "-");
-
-const nav = document.getElementsByClassName("navigation");
-
 const selectedNavItem = document.getElementById(pageTitle);
 
 if (selectedNavItem) {
   selectedNavItem.classList.add("selected");
+  displaySubNavElem(selectedNavItem);
 }
 
-// finds external links and attaches an arrow icon
+function displaySubNavElem(selectedNavItem) {
+  const navItems = document.querySelectorAll(".navigation a");
+  const parentPageId = selectedNavItem.classList.contains("sub-navigation")
+    ? selectedNavItem.getAttribute("href").split("/")[1]
+    : selectedNavItem.getAttribute("id");
+
+  for (const navItem of navItems) {
+    if (
+      navItem.classList.contains("sub-navigation") &&
+      navItem.getAttribute("href").includes(parentPageId)
+    ) {
+      navItem.style.display = "flex";
+    }
+  }
+}
+
+// Finds external links and attaches an arrow icon
 const allAnchors = document.getElementsByTagName("a");
 
 for (const anchor of allAnchors) {
+  const href = anchor.getAttribute("href");
+
   if (
-    anchor.getAttribute("href").includes("http") 
-    || anchor.getAttribute("href").includes(".pdf") &&
-    !anchor.parentNode.classList.contains("publication-item")
+    href.includes("http") ||
+    (href.includes(".pdf") && !anchor.parentNode.classList.contains("publication-item"))
   ) {
     anchor.setAttribute("target", "_blank");
     anchor.classList.add("external-link");
   }
-}
-
-// const contentAnchors = document.querySelectorAll(".franklin-content a");
-
-// var subNavigation = document.createElement('div');
-// subNavigation.className = 'sub-navigation';
-
-// for (const anchor of contentAnchors) {
-
-//   if (anchor.getAttribute("href").includes(pageTitle +"/")) {
-//     const title = anchor.innerHTML;
-//     console.log(title);
-//     var subNavigationItem = document.createElement('a');
-//     subNavigationItem.setAttribute('href', anchor.getAttribute("href"));
-//     subNavigationItem.appendChild(document.createTextNode(title));
-//     subNavigation.appendChild(subNavigationItem);
-//     selectedNavItem.after(subNavigation);
-//   }
-
-// }
-
-// find internal links and attaches them to navigation bar
-
-function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-var subNavElems = document.querySelectorAll(".sub-navigation");
-
-const navDiv = document.getElementsByClassName("navigation")[0];
-
-for (const subNavElem of subNavElems) {
-  const parentPage = subNavElem.getAttribute("href").split("/")[1];
-  console.log(parentPage);
-  const parentPageNavElem = document.getElementById(parentPage);
-  console.log(parentPageNavElem);
-
- 
-  navDiv.removeChild(subNavElem);
-
-  // console.log(parentPageNavElem.nextElementSibling);
-  navDiv.insertBefore(subNavElem, parentPageNavElem.nextElementSibling)
-  // insertAfter(parentPageNavElem, subNavElem);
 }
--- presentations.md
+++ presentations.md
@@ -7,4 +7,4 @@
 - Waves in Time-Varying Media 2023 (invited talk)
 - Waves in Time-Varying Media 2021 (OSA best poster award)
 
-See [CV](/assets/jamison_CV.pdf) for a full list of conference presentations.
+<!-- See [CV](/assets/jamison_CV.pdf) for a full list of conference presentations. -->
